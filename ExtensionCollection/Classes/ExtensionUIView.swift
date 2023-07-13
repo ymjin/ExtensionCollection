43b76eddb5d8c15bf2dd6ca00de27934ca8dffdf
ymjin <ymjin.mv@movements.kr>
2023-07-13 09:28:52 +0900

    0.3.6

--- ExtensionCollection/Classes/ExtensionUIView.swift
+++ ExtensionCollection/Classes/ExtensionUIView.swift
@@ -32,16 +32,3 @@ extension UIView {
         layer.changeBorder(width: width, radius: radius, color: color)
     }
 }
-
-extension CALayer {
-    /// 보더 라인 그리기
-    /// - Parameters:
-    ///   - width: 너비
-    ///   - radius: 반경
-    ///   - color: 색상
-    public func changeBorder(width: CGFloat = 1, radius: CGFloat = 10, color: UIColor = UIColor(red: 194/255, green: 194/255, blue: 194/255, alpha: 1)) {
-        cornerRadius = radius
-        borderWidth = width
-        borderColor = color.cgColor
-    }
-}
