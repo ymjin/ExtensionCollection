9af635777fbd0d55c1c6e2f3cba7f905d9f48e7e
ymjin <ymjin.mv@movements.kr>
2023-07-13 09:58:18 +0900

    0.3.8

--- ExtensionCollection/Classes/UIExtension/ExtensionCALayer.swift
+++ ExtensionCollection/Classes/UIExtension/ExtensionCALayer.swift
@@ -13,7 +13,7 @@ extension CALayer {
     ///   - width: 너비
     ///   - radius: 반경
     ///   - color: 색상
-    public func changeBorder(width: CGFloat = 1, radius: CGFloat = 10, color: UIColor = UIColor(red: 194/255, green: 194/255, blue: 194/255, alpha: 1)) {
+    func changeBorder(width: CGFloat = 1, radius: CGFloat = 10, color: UIColor = UIColor(red: 194/255, green: 194/255, blue: 194/255, alpha: 1)) {
         cornerRadius = radius
         borderWidth = width
         borderColor = color.cgColor
